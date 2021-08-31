5ecaa3f1499ed0d15bfdbcde2f8a54e8a589a249
oiler <jrf1039@gmail.com>
2021-08-31 14:19:44 -0400

    update modal

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,5 +1,6 @@
 /***
  * src: https://dev.to/dailydevtips1/vanilla-javascript-modal-pop-up-2oki
+ * additional: https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
  ***/
 
 function content_modal() {
@@ -11,11 +12,19 @@ function content_modal() {
         event.preventDefault();
         const modal = document.getElementById(trigger.dataset.modal);
         modal.classList.add("open");
+        
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${window.scrollY}px`;
+
         const exits = modal.querySelectorAll(".modal-exit");
         exits.forEach(function (exit) {
           exit.addEventListener("click", function (event) {
             event.preventDefault();
             modal.classList.remove("open");
+            const scrollY = document.body.style.top;
+            document.body.style.position = '';
+            document.body.style.top = '';
+            window.scrollTo(0, parseInt(scrollY || '0') * -1);
           });
         });
       });
